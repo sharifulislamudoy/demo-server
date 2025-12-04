c94715613c4e856d507dad929cd34b14fd256f3d
JI-RIDOY <aridoy102536@gmail.com>
2025-12-04 16:54:44 +0600

    hi

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const port = 5000;
 
-// middleware to parse json
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+// MongoDB connection
+const uri = process.env.MONGO_URI || "YOUR_MONGODB_URI_HERE";
+
+mongoose.connect(uri, {
+  dbName: "testDB"
+})
+.then(() => {
+  console.log("MongoDB connected");
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err.message);
 });
 
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running with MongoDB');
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
